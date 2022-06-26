9125a8cc793807a303ab0dcbd732cdbec50d6c88
Christopher Conroy <39525900+cbc02009@users.noreply.github.com>
2022-06-26 08:10:33 -0400

    add command line parsing to generator.

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,84 @@
+use std::collections::HashSet;
 use std::fs;
-use std::fs::OpenOptions;
 use std::io::Read;
+use std::path::PathBuf;
 use serde::{Serialize, Deserialize};
+use clap::Parser;
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
-struct Config {
-    template: String,
-    output: String,
-    replace: Vec<ReplaceValue>,
+#[derive(Clone, Debug, Parser)]
+#[clap(author, version)]
+struct CliConfig {
+
+    /// Path for template file
+    #[clap(short, long)]
+    template: PathBuf,
+
+    /// Path for file containing replacement values
+    #[clap(short, long)]
+    values: PathBuf,
+
+    /// Path for output file
+    #[clap(short, long)]
+    output: PathBuf,
+
+    /// Optional override for "app" param
+    #[clap(short, long)]
+    app: Option<String>,
+
+    /// Optional override for "namespace" param
+    #[clap(short, long)]
+    namespace: Option<String>,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
+struct Config {
+    replace: HashSet<ReplaceValue>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize, Hash)]
 struct ReplaceValue {
     param: String,
     value: String,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // TODO add clap or similar
+
+    // Read in CLI commands
+    let cli: CliConfig = CliConfig::parse();
+
+    // dbg!(cli.clone());
 
     //read in template
     let mut template = String::new();
-    let mut tf = std::fs::File::open("template.yaml")?;
+    let mut tf = std::fs::File::open(cli.template)?;
     tf.read_to_string(&mut template)
-        .expect("Could not read file");
+        .expect("Could not read template file");
 
-    dbg!(template.clone());
+    // dbg!(template.clone());
 
     // read in values
-    let mut vf = std::fs::File::open("values.yaml")?;
-    let mut values: Config = serde_yaml::from_reader(vf)?;
+    let vf = std::fs::File::open(cli.values)?;
+    let values: Config = serde_yaml::from_reader(vf)?;
 
-    dbg!(values.clone());
+    // dbg!(values.clone());
+
+    // Replace custom values from CLI first
+    if let Some(app) = cli.app {
+        template = template.replace("@@app@@", &*app);
+    }
+    if let Some(ns) = cli.namespace {
+        template = template.replace("@@namespace@@", &*ns)
+    }
 
     let replaces = values.replace;
     for rv in replaces {
-        template = template.replace(&*rv.param, &*rv.value);
+        let rs = format!("@@{}@@", &*rv.param);
+        template = template.replace(&*rs, &*rv.value);
     }
 
-    dbg!(template.clone());
+    // dbg!(template.clone());
 
-    fs::write("output.yaml", template)?;
+    fs::write(cli.output, template)?;
 
     Ok(())
 
